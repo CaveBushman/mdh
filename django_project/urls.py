39151389c08a633a1f0d3efc003492b74fb4eb76
CaveBushman <david@black-ops.eu>
2023-05-09 16:56:41 +0200

    small upgrade

--- django_project/urls.py
+++ django_project/urls.py
@@ -25,9 +25,8 @@ urlpatterns = [
     path('cases/', include('cases.urls'))
 ]
 
-if settings.DEBUG: 
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "MDH Legal s.r.o."
 admin.site.index_title = "Management"
